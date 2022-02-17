0d243f361acd5004240eb5a9d1ecac2d75c68404
samu <samu13lop@gmail.com>
2022-02-17 11:51:48 +0100

    Practica 1 terminada

---
+++ Practica1/DVDCine/src/GestionaDVDCine.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class GestionaDVDCine {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Título de la película");
+        String titulo = sc.nextLine();
+        System.out.print("Actores principales: ");
+        String actores = sc.nextLine();
+        System.out.print("Director: ");
+        String director = sc.nextLine();
+        System.out.print("Genero: ");
+        String genero = sc.nextLine();
+        System.out.print("Duración en minutos: ");
+        int dura = sc.nextInt();
+        sc.nextLine();
+        System.out.println("Resumen:");
+        String resumen = sc.nextLine();
+        DVDCine peli = new DVDCine(titulo,
+                director, actores, dura, resumen, genero);
+        System.out.println(peli.muestraDVDCine());
+        System.out.println(peli.esThriller()
+                ? "Es un thriller" : "No es un thriller");
+        if (peli.tieneResumen()) {
+            System.out.println("La ficha tiene resumen");
+        }
+    }
+}
