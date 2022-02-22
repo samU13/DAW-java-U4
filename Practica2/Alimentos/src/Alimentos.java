c781f8a1e58b4cf10b3cc5da06160ff2e806aaba
samu <samu13lop@gmail.com>
2022-02-22 15:01:10 +0100

    new practice

---
+++ Practica2/Alimentos/src/Alimentos.java
@@ -0,0 +1,99 @@
+public class Alimentos {
+
+    String nombre;
+    int lipidos;
+    int hidratos;
+    int proteinas;
+    boolean animal;
+
+
+    String vitaminas;
+    String minerales;
+
+    Alimentos(String nombre){ this.nombre=nombre;}
+
+    Alimentos(int lipidos, int hidratos, int proteinas, boolean animal, String vitaminas, String minerales){
+        this.lipidos=lipidos;
+        this.hidratos=hidratos;
+        this.proteinas=proteinas;
+        this.animal=animal;
+        this.vitaminas=vitaminas;
+        this.minerales=minerales;
+    }
+
+    public String getNombre() {return nombre;}
+
+    public int getLipidos() {return lipidos;}
+
+    public int getHidratos() {return hidratos;}
+
+    public int getProteinas() {return proteinas;}
+
+    public boolean isAnimal() {return animal;}
+
+    public String getVitaminas() {return vitaminas;}
+
+    public String getMinerales() {return minerales;}
+
+
+
+    public boolean esDietetico(){
+        int lipidos= getLipidos();
+        String vitaminas= getVitaminas();
+        return lipidos < 20 && !vitaminas.equals("B");
+    }
+
+    public double calculaContenidoEnergetico (){
+        double lipidos = getLipidos();
+        double proteinas = getProteinas();
+        double hidratos= getHidratos();
+        return ((lipidos/10)*9.4)+((proteinas/10)*5.3)+((hidratos/10)*4.1);
+
+    }
+
+
+    public boolean esRecomendableParaDeportistas (){
+        int proteinas= getProteinas();
+        int hidratos= getHidratos();
+        int lipidos= getLipidos();
+        return ((proteinas >= 10) && (proteinas <= 15)) && ((lipidos >= 30) && (lipidos <= 35)) && ((hidratos >= 55) && (hidratos <= 65));
+    }
+
+    public String codigoV(){
+        if(getVitaminas().equals("A")||getVitaminas().equals("a")){
+            return "Alto";
+        }if(getVitaminas().equals("B")||getVitaminas().equals("b")){
+            return "Bajo";
+        }else{
+            return "Medio";
+        }
+
+    }
+    public String codigoM(){
+        if(getMinerales().equals("A")||getMinerales().equals("a")){
+            return "Alto";
+        }if(getMinerales().equals("B")||getMinerales().equals("b")){
+            return "Bajo";
+        }else{
+            return "Medio";
+        }
+
+    }
+
+    public String muestraAlimento() {
+        if (isAnimal()) {
+            return "El " + getNombre() + " es un alimento que tiene un " + getLipidos() + "% de lipidos, un " + getProteinas() + "% de" +
+                    " proteinas y un " + getHidratos() + "% de hidratos. Asi mismo tiene un " + codigoV() + " contenido en" +
+                    " vitaminas y un " + codigoM() + " contenido en minerales. Ademas es de origen animal";
+        } else {
+            return "El " + getNombre() + " es un alimento que tiene un " + getLipidos() + "% de lipidos, un " + getProteinas() + "% de" +
+                    " proteinas y un " + getHidratos() + "% de hidratos. Asi mismo tiene un " + codigoV() + " contenido en" +
+                    " vitaminas y un " + codigoM() + " contenido en minerales. No es de origen animal";
+        }
+    }
+
+}
+
+
+
+
---
+++ Practica2/Alimentos/src/pruebaAlimento.java
@@ -0,0 +1,50 @@
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class pruebaAlimento {
+
+    public static void main(String[] args){
+        Alimentos a1;
+        Scanner teclado=new Scanner(System.in);
+
+        System.out.println("NECESITAREMOS LOS SIGUIENTES DATOS DE UN ALIMENTO :");
+        System.out.println("Introduce su nombre : ");
+        String nombre= teclado.nextLine();
+        System.out.println("Introduce su contenido en lipidos sin % : ");
+        int lipidos= teclado.nextInt();
+        System.out.println("Introduce su contenido en hidratos sin % : ");
+        int hidratos= teclado.nextInt();
+        System.out.println("Introduce su contenido en proteinas sin % : ");
+        int proteinas= teclado.nextInt(); teclado.nextLine();
+        System.out.println("Su contenido en minerales: A alto, M medio, B bajo : ");
+        String minerales= teclado.nextLine();
+        System.out.println("Su contenido en en vitaminas: A alto, M medio, B bajo : ");
+        String vitaminas= teclado.nextLine();
+        System.out.println("Es de origen animal: 1.Afirmativo 2.Negativo : ");
+        int anim= teclado.nextInt();
+        boolean animal;
+        animal= anim == 1;
+
+        a1 = new Alimentos(nombre);
+        a1 = new Alimentos(lipidos,hidratos,proteinas,animal,vitaminas,minerales);
+
+        System.out.println(a1.muestraAlimento());
+        if(a1.esDietetico()){
+            System.out.println("Es un producto dietetico.");
+        }else {
+            System.out.println("No es un producto dietetico.");
+        }
+
+        if(a1.esRecomendableParaDeportistas()){
+            System.out.println("Es recomendable para deportistas.");
+        }else{
+            System.out.println("No es recomendable para deportistas.");
+        }
+
+        DecimalFormat df= new DecimalFormat("#.00");
+
+        System.out.print("El contenido energetico de 1g del alimentos es de: "+df.format(a1.calculaContenidoEnergetico())+" Kcal");
+
+
+    }
+}
