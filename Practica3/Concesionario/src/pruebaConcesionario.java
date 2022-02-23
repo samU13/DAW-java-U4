0174824513abec50b6793fbe7fb71bb668bddd7b
samu <samu13lop@gmail.com>
2022-02-23 15:02:34 +0100

    Ejercicio concesionario

--- Practica3/Concesionario/src/pruebaConcesionario.java
+++ Practica3/Concesionario/src/pruebaConcesionario.java
@@ -1,3 +1,4 @@
+import java.util.Locale;
 import java.util.Scanner;
 
 public class pruebaConcesionario {
@@ -9,8 +10,8 @@ public class pruebaConcesionario {
         arr= new Vehiculo[1000];
         for(int i=0;i<=1000;i++){
             System.out.println("Introduce el modelo del coche: ");
-            modelo= teclado.nextLine();
-            if(modelo.equals("Fin")){
+            modelo= teclado.nextLine().toUpperCase();
+            if(modelo.equals("FIN")){
                 System.out.println("Fin del programa");
                 break;
             }
@@ -18,9 +19,14 @@ public class pruebaConcesionario {
             potencia= teclado.nextDouble();teclado.nextLine();
             arr[i]= new Vehiculo(modelo, potencia);
         }
-        for (int i = 0; i < arr.length; i++)
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == null) {
+                break;
+            }
             System.out.println("El coche numero " + i + " : " +
                     arr[i].modelo +" "+ arr[i].potencia+" cv");
+        }
     }
 
 }
