bd3fd8fe3e65b49b97461430cda7d731e9bb61c6
samU13 <samu13lop@gmail.com>
2022-04-27 19:18:33 +0200

    update

--- Practica3/Cronometro/src/Cronometro.java
+++ Practica3/Cronometro/src/Cronometro.java
@@ -1,30 +1,8 @@
-public class Cronometro {
-
-    long start;
-    long end;
-
-    Cronometro(){
-
-    }
-    Cronometro(long start, long end){
-        this.start=start;
-        this.end=end;
-    }
-
-    public long sStart(){
-        return System.currentTimeMillis();
-    }
-
-    public long sEnd(){
-        return System.currentTimeMillis();
-    }
-
-    public long esTiempo(long start, long end){
-        return end-start;
+    public class Cronometro {
+        Cronometro(){}
+        public long sStart(){return System.currentTimeMillis();}
+        public long sEnd(){return System.currentTimeMillis();}
+        public long esTiempo(long start, long end){ return end-start;}
     }
 
 
-
-
-
-}
--- Practica3/Cronometro/src/pruebaCronometro.java
+++ Practica3/Cronometro/src/pruebaCronometro.java
@@ -2,20 +2,17 @@ import java.util.Scanner;
 
 public class pruebaCronometro {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner teclado=new Scanner(System.in);
-        Cronometro c1= new Cronometro();
+        Cronometro c1=new Cronometro();
         long start;
         System.out.println("VAMOS CALCULAR EL TIEMPO ENTRE DOS PULSACIONES!");
-        System.out.println("Pulsa la tecla INTRO: ");
+        System.out.println("Pulsa la tecla INTRO para empezar! ");
         teclado.nextLine();
-        start=c1.sStart();
-        System.out.println("Pulsa la tecla INTRO para finalizar: ");
+        start= c1.sStart();
+        System.out.println("Pulsa la tecla INTRO para finalizar!");
         teclado.nextLine();
         long end = c1.sEnd();
-        System.out.println("El tiempo que paso entre las dos pulsaciones es de : "+c1.esTiempo(start,end));
-
-
+        System.out.println("El tiempo que paso entre las dos pulsaciones es de "+c1.esTiempo(start,end)+" ms");
     }
-
 }
