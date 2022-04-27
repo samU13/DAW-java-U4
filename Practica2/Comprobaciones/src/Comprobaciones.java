a63521a0304e64a9800e725c2f93ba0921887b74
samU13 <samu13lop@gmail.com>
2022-04-27 17:34:49 +0200

    update

--- Practica2/Comprobaciones/src/Comprobaciones.java
+++ Practica2/Comprobaciones/src/Comprobaciones.java
@@ -1,79 +1,55 @@
 public class Comprobaciones {
-
-
-    public boolean esPrimo(int num){
-
-        if(num == 0 || num== 1 || num == 4){
-            return false;
-        }
-        for(int x = 2; x < num /2; x++){
-            if (num % x == 0)
+    public boolean esPrimo(int num) {
+        for (int x = 2; x < num / 2; x++) {
+            if (num % x == 0) {
                 return false;
+            }
         }
-    return true;
+        return true;
     }
 
-    public boolean esAntiguo(int num){
-        if(num <= 1900){
-            return true;
-        }else{
-            return false;
-        }
+    public boolean esAntiguo(int num) {
+        return num <= 1900;
     }
 
-    public boolean esPar(int num){
-        if(num%2==0){
-            return true;
-        }else{
-            return false;
-        }
+    public boolean esPar(int num) {
+        return num % 2 == 0;
     }
 
-    public boolean esPositivo(int num){
-        if(num>0){
-            return true;
-        }else{
-            return false;
-        }
+    public boolean esPositivo(int num) {
+        return num > 0;
     }
 
-    public boolean esBisiesto(int num){
-        if((num % 4 == 0) && ((num % 100 != 0) || (num% 400 == 0))){
-            return true;
-        }else{
-            return false;
-        }
+    public boolean esBisiesto(int num) {
+        return (num % 4 == 0 && num % 100 != 0) || (num % 400 == 0);
     }
 
-    public boolean esCapicua(int num){
-        int falta=num;
-        int resto=0;
-        int numInvertido=0;
-        while(falta!=0){
-            resto=falta%10;
-            numInvertido=numInvertido*10+resto;
-            falta=falta/10;
+    public boolean esCapicua(int num) {
+        int falta = num;
+        int resto = 0;
+        int numInvertido = 0;
+        while (falta != 0) {
+            resto = falta % 10;
+            numInvertido = numInvertido * 10 + resto;
+            falta = falta / 10;
         }
-        if(numInvertido==num){
-            return true;
-        }else{
-            return false;
-        }
+        return numInvertido == num;
     }
 
-    public boolean esIntervalo(int num1, int num2, int num3){
-        if(num1>num2 && num1<num3){
-            return true;
-        }else{
-            return false;
-        }
+    public boolean esIntervalo(int num1, int num2, int num3) {
+        return num1 > num2 && num1 < num3;
     }
 
-    public boolean esTriangulo(int num1, int num2, int num3){
-        if(num1>=(num2+num3) || num2>=(num1+num3) || num3>=(num1+num2)){
-            return false;
-        }else{
-            return true;
+    public boolean esTriangulo(int num1, int num2, int num3) {
+        return num1 >= (num2 + num3) || num2 >= (num1 + num3) || num3 >= (num1 + num2);
+
+    }
+    public boolean esSimetrico(String texto) {
+        for (int i = 0; i < texto.length()/2; i++) {
+            if (texto.charAt(i) != texto.charAt(texto.length() - i - 1)) {
+                return false;
+            }
         }
+        return true;
     }
 }
--- Practica2/Comprobaciones/src/PruebaComprobaciones.java
+++ Practica2/Comprobaciones/src/PruebaComprobaciones.java
@@ -1,9 +1,7 @@
 import java.util.Scanner;
 
 public class PruebaComprobaciones {
-
-    public static void main(String[] args){
-
+    public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.println("Introduce un numero entero: ");
         int num = sc.nextInt();
@@ -19,8 +17,13 @@ public class PruebaComprobaciones {
         int num1 = sc.nextInt();
         int num2 = sc.nextInt();
         int num3 = sc.nextInt();
+        sc.nextLine(); //permite introducir el siguiente scan
         System.out.println("Los numeros indicados pueden formar un triangulo: "+c1.esTriangulo(num1, num2, num3));
         System.out.println("El primero numero esta en entre "+num2+" y "+num3+" -> "+c1.esIntervalo(num1, num2, num3));
 
+        System.out.println("Introduce un numero entero o texto: ");
+        String texto = sc.nextLine();
+        System.out.println("El numero o texto introducido es simetrico -> "+c1.esSimetrico(texto));
     }
+
 }
