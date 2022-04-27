4bf6bfbc296f119336f231061a9523ed1c05e0e3
samU13 <samu13lop@gmail.com>
2022-04-27 19:53:57 +0200

    update

--- Practica3/Concesionario/src/Vehiculo.java
+++ Practica3/Concesionario/src/Vehiculo.java
@@ -3,23 +3,29 @@ public class Vehiculo {
     double potencia;
     boolean cRuedas;
 
-    Vehiculo( String modelo, double potencia){this.modelo=modelo; this.potencia=potencia;}
-
-
+    Vehiculo(String modelo, double potencia) {
+        this.modelo = modelo;
+        this.potencia = potencia;
+    }
 
     public double getPotencia() {
         return potencia;
     }
 
+
     public boolean iscRuedas() {
         return cRuedas;
     }
 
-    public String imprimir(){
-        if( iscRuedas()){
-            return "Modelo: "+modelo+" /nPotencia: "+getPotencia()+ "/nTraccion a las cuatro ruedas";
-        }else{
-            return "Modelo: "+modelo+" /nPotencia: "+getPotencia()+ "/nSin traccion a las cuatro ruedas";
+    public void setcRuedas(boolean cRuedas) {
+        this.cRuedas = cRuedas;
+    }
+
+    public String Imprimir() {
+        if (iscRuedas()) {
+            return "Modelo: " + modelo + "\n" + "Potencia: " + getPotencia() + "\n" + "Traccion a las cuatro ruedas";
+        } else {
+            return "Modelo: " + modelo + "\n" + "Potencia: " + getPotencia() + "\n" + "Sin traccion a las cuatro ruedas";
         }
     }
 }
--- Practica3/Concesionario/src/pruebaConcesionario.java
+++ Practica3/Concesionario/src/pruebaConcesionario.java
@@ -5,32 +5,37 @@ public class pruebaConcesionario {
         Scanner teclado = new Scanner(System.in);
         String modelo;
         double potencia;
-        Vehiculo[] arr;
+        boolean cRuedas;
+        Vehiculo[]arr;
         arr = new Vehiculo[1000];
         System.out.println("PROGRAMA CONCESIONARIO. INTRODUZCA LOS COCHES (FIN SI QUIERES ACABAR)");
-        for (int i = 0; i <= 1000; i++) {
+        for (int i=0; i<=1000;i++){
             System.out.println("Introduce el modelo del coche: ");
-            modelo = teclado.nextLine().toUpperCase();
-            if (modelo.equals("FIN")) {
+            modelo=teclado.nextLine().toUpperCase();
+            if(modelo.equals("FIN")){
                 System.out.println("Fin del programa");
                 break;
             }
             System.out.println("Introduce potencia del coche: ");
-            potencia = teclado.nextDouble();
+            potencia=teclado.nextDouble();
             teclado.nextLine();
-            arr[i] = new Vehiculo(modelo, potencia);
+
+            System.out.println("Tiene traccion a las cuatro ruedas? 1.Afirmativo 2.Negativo");
+            int cRue = teclado.nextInt();
+            cRuedas= cRue == 1;
+            arr[i]= new Vehiculo(modelo,potencia);
+            arr[i].setcRuedas(cRuedas);
+            teclado.nextLine();
         }
-        System.out.println("Los coches disponibles en el concesionario son : ");
-        for (int i = 0; i < arr.length; i++) {
-          //for (int i=0; !arr[i] == null; i++)
-            if (arr[i] == null) {
+        System.out.println("Los coches disponibles en el concesionario son: ");
+        for(int i=0; i<arr.length; i++){
+            if (arr[i]==null) {
                 break;
             }
-            System.out.println("El coche numero " + i + " : "
-                    + arr[i].modelo + " " + arr[i].potencia + " cv");
-            
-            //System.out.println(arr[i].imprimir());
+            //System.out.println("El coche numero " + i + " : "
+            // + arr[i].modelo + " " + arr[i].potencia + " cv");
+
+            System.out.println("El coche numero "+i+" : "+"\n"+arr[i].Imprimir());
         }
     }
-
 }
