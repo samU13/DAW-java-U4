25bbb25436582e5f9920f162d43ee10a13d1114b
samU13 <samu13lop@gmail.com>
2022-04-28 10:42:45 +0200

    update

--- PracticaBanco/src/Banco.java
+++ PracticaBanco/src/Banco.java
@@ -1,37 +1,28 @@
 import java.util.Scanner;
 
 public class Banco {
-    private static Cliente clientes[]=new Cliente[1000];
+    private static final Cliente[] clientes =new Cliente[1000];
     private static int numSigCliente=0;
-    private static Cuenta cuentas[]=new Cuenta[1000];
+    private static final Cuenta[] cuentas =new Cuenta[1000];
     private static int numSigCuenta=1;
     private static double comisionMensual=0.6;
-    private static Scanner teclado = new Scanner(System.in);
+    private static final Scanner teclado = new Scanner(System.in);
 
     public static void main(String[] args) {
 
-        int opcion=0;
+        int opcion;
         do{
             opcion=mostrarMenu();
-            switch (opcion){
-                case 1: ingreso();
-                    break;
-                case 2: cargo();
-                    break;
-                case 3: consultarSaldo();
-                    break;
-                case 4: cambioPropietario();
-                    break;
-                case 6: nuevoCliente();
-                    break;
-                case 5: revisionMensual();
-                    break;
-                case 8: listarClientes();
-                    break;
-                case 7: nuevaCuenta();
-                    break;
-                case 9: cambiarComisionMensual();
-                    break;
+            switch (opcion) {
+                case 1 -> ingreso();
+                case 2 -> cargo();
+                case 3 -> consultarSaldo();
+                case 4 -> cambioPropietario();
+                case 6 -> nuevoCliente();
+                case 5 -> revisionMensual();
+                case 8 -> listarClientes();
+                case 7 -> nuevaCuenta();
+                case 9 -> cambiarComisionMensual();
             }
 
         }while (opcion!=0);
@@ -90,7 +81,7 @@ public class Banco {
 
     static void nuevaCuenta(){
         String nif;
-        int opcion=0;
+        int opcion;
         double saldo;
 
         System.out.println("Introduzca tipo de cuenta");
@@ -102,50 +93,43 @@ public class Banco {
         opcion=teclado.nextInt();
         teclado.nextLine();
 
-        switch (opcion)
-        {
-            case 1:
+        switch (opcion) {
+            case 1 -> {
                 System.out.println("Introduzca N.I.F. del cliente");
-                nif=teclado.nextLine();
-                if (localizarCliente(nif)!=null) {
+                nif = teclado.nextLine();
+                if (localizarCliente(nif) != null) {
                     System.out.println("Introduzca saldo");
-                    saldo=teclado.nextDouble();
+                    saldo = teclado.nextDouble();
                     teclado.nextLine();
-                    cuentas[numSigCuenta] = new CuentaCorriente(saldo,numSigCuenta,localizarCliente(nif));
+                    cuentas[numSigCuenta] = new CuentaCorriente(saldo, numSigCuenta, localizarCliente(nif));
                     numSigCuenta++;
-                }
-                else
+                } else
                     System.out.println("El cliente no existe");
-                break;
-            case 2:
+            }
+            case 2 -> {
                 System.out.println("Introduzca N.I.F. del cliente");
-                nif=teclado.nextLine();
-                if(localizarCliente(nif)!=null)
-                {
+                nif = teclado.nextLine();
+                if (localizarCliente(nif) != null) {
                     System.out.println("Introduzca saldo");
-                    saldo=teclado.nextDouble();
+                    saldo = teclado.nextDouble();
                     teclado.nextLine();
-                    cuentas[numSigCuenta] = new CuentaVivienda(saldo,numSigCuenta,localizarCliente(nif));
+                    cuentas[numSigCuenta] = new CuentaVivienda(saldo, numSigCuenta, localizarCliente(nif));
                     numSigCuenta++;
-                }
-                else
+                } else
                     System.out.println("El cliente no existe");
-                break;
-
-            case 3:
+            }
+            case 3 -> {
                 System.out.println("Introduzca N.I.F. del cliente");
-                nif=teclado.nextLine();
-                if(localizarCliente(nif)!=null)
-                {
+                nif = teclado.nextLine();
+                if (localizarCliente(nif) != null) {
                     System.out.println("Introduzca saldo");
-                    saldo=teclado.nextDouble();
+                    saldo = teclado.nextDouble();
                     teclado.nextLine();
-                    cuentas[numSigCuenta] = new FI(saldo,numSigCuenta,localizarCliente(nif));
+                    cuentas[numSigCuenta] = new FI(saldo, numSigCuenta, localizarCliente(nif));
                     numSigCuenta++;
-                }
-                else
+                } else
                     System.out.println("El cliente no existe");
-                break;
+            }
         }
     }
 
@@ -158,6 +142,98 @@ public class Banco {
         }
     }
 
+    static void ingreso(){
+        int i, numCta;
+        double importe;
 
+        System.out.println("Introduzca el numero de cuenta: ");
+        numCta= teclado.nextInt();
+        teclado.nextLine();
+        for(i=1;i<numSigCuenta;i++){
+            if(numCta==cuentas[i].getNumeroCuenta()) {
+                System.out.println("Introduzca el importe del ingreso");
+                importe = teclado.nextDouble();
+                teclado.nextLine();
+                cuentas[i].ingresar(importe);
+                System.out.println("El saldo actual de la cuenta es de " + cuentas[i].getSaldo());
+                return;
+            }
+            System.out.println("No existe ninguna cuenta con el numero: "+numCta);
+        }
+
+
+    }
+    static void cargo(){
+        int i, numCta;
+        double importe;
+
+        System.out.println("Introduzca el numero de cuenta: ");
+        numCta= teclado.nextInt();
+        teclado.nextLine();
+        for(i=1;i<numSigCuenta;i++){
+            if(numCta==cuentas[i].getNumeroCuenta()){
+                System.out.println("Introduzca el importe del cargo: ");
+                importe=teclado.nextDouble();
+                teclado.nextLine();
+                cuentas[i].retirar(importe);
+                System.out.println("El saldo actual de la cuenta es de " + cuentas[i].getSaldo());
+                return;
+            }
+            System.out.println("No existe ninguna cuenta con el numero: "+numCta);
+        }
+    }
+
+    static void consultarSaldo(){
+        int i, numCta;
+        System.out.println("Introduzca el numero de cuenta: ");
+        numCta= teclado.nextInt();
+        teclado.nextLine();
+        for(i=1;i<numSigCuenta;i++) {
+            if (numCta == cuentas[i].getNumeroCuenta()) {
+                System.out.println("El saldo actual de la cuenta es de " + cuentas[i].getSaldo());
+            }
+        }
+        System.out.println("No existe ninguna cuenta con el numero: "+numCta);
+    }
+
+    static void cambioPropietario(){
+        int i, numCta;
+        String nif;
+
+        System.out.println("Introduzca el numero de cuenta: ");
+        numCta= teclado.nextInt();
+        teclado.nextLine();
+        for(i=1;i<numSigCuenta;i++) {
+            if (numCta == cuentas[i].getNumeroCuenta()) {
+                System.out.println("Introduzca N.I.F. del nuevo propietario: ");
+                nif=teclado.nextLine();
+                if(localizarCliente(nif)!=null){
+                    cuentas[i].setCliente(localizarCliente(nif));
+                }else {
+                    System.out.println("No existe ningun cliente con este N.I.F.");
+                    return;
+                }
+                return;
+            }
+
+            }
+        System.out.println("No existe ninguna cuenta con el numero: "+numCta);
+    }
+
+    static void revisionMensual(){
+        int i;
+
+        for(i=1; i<numSigCuenta; i++){
+            cuentas[i].pagarIntereses();
+            cuentas[i].aplicarComision(comisionMensual);
+        }
+    }
+
+    static void cambiarComisionMensual(){
+        System.out.println("Introduzca la nueva comision mensual: ");
+        comisionMensual=teclado.nextDouble();
+        teclado.nextLine();
+        System.out.println("La nueva comision mensual es de: "+comisionMensual);
+    }
 
 }
